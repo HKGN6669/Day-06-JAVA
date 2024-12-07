e4739b0768e1b12562043ac05778c356351d610d
HKGN6669 <tayyibafathima666@gmail.com>
2024-12-07 11:39:34 +0530

    Add files via upload

---
+++ breakstmt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class breakstmt{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            if(i==2)
+            break;
+            System.out.println(i);
+        }
+    }
+}        
---
+++ continuestmt.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class continuestmt{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            if(i==2)
+            continue;
+            System.out.println(i);
+        }
+    }
+}        
+
+
